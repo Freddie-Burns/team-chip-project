aaea9e3d68b5fc4f7b9e7b68652fe7b64272b3cc
Freddie Burns <freddieburns@hotmail.co.uk>
2023-11-23 12:33:06 +0000

    Create script for testing device connections

---
+++ instrument_connections.py
@@ -0,0 +1,19 @@
+import pyvisa
+
+rm = pyvisa.ResourceManager('@py')
+print(rm.list_resources())
+
+laser = rm.open_resource("ASRL7::INSTR")
+laser.write_termination = '\r'
+laser.read_termination = '\r'
+laser.baud_rate = 9600
+laser.data_bits = 8
+
+response = laser.query('*IDN?', delay=1)
+print(response)
+laser.close()
+
+powermeter = rm.open_resource('USB0::4883::32888::P0001012::0::INSTR')
+powermeter.write_termination = '\r'
+powermeter.read_termination = '\r'
+print(powermeter.query('*IDN?'))
