5ace57245edf9ad2bdbea42954bd263d25813ba0
Freddie Burns <freddieburns@hotmail.co.uk>
2023-12-09 17:01:36 +0000

    Adjust transmission spectra
    Give each transmission spectrum the same maximum as the highest transmitting spectrum. Usually allows easier comparison of spectra.

--- lightrig/resonance_plotter.py
+++ lightrig/resonance_plotter.py
@@ -7,16 +7,19 @@ import matplotlib.pyplot as plt
 
 
 COLOURS = ['b', 'g', 'r', 'c', 'm', 'y']
-DWDM_CHANNELS = (32, 33, 35)  # Channels to show transmission data
+DWDM_CHANNELS = (24, 31, 37)  # channels to show transmission data
 DWDM_DIR = Path("../data/dwdm")
 RESONANCE_DIR = Path("../data/resonance")
-Y_LIM = (-20,-17)
+X_LIM = (1545, 1560)
+Y_LIM = (-19, -15)
 
 # Usually only plot one resonance file
 # Can plot more than one to compare
 # e.g. to see if resonances shift over time
 FILES = [
-    "ring-17 2023-12-01 10-49-28.csv",
+    # "ring-17 2023-12-01 10-49-28.csv",
+    "ring-01 2023-12-01 10-40-52.csv",
+    "ring-01 2023-12-04 13-50-17.csv",
 ]
 
 
@@ -24,6 +27,7 @@ def main():
     # Create matplotlib plot and set y-axis limits
     ax = plt.subplot()
     plt.ylim(Y_LIM)
+    plt.xlim(X_LIM)
     plot_channel_centers(ax)
     plot_transmission_data(ax)
     plot_dwdm_transmission(ax)
@@ -34,26 +38,42 @@ def plot_channel_centers(ax):
     """Plot all DWDM channel central wavelengths as dotted lines."""
     channels = pd.read_csv(DWDM_DIR / "channel wavelength table.csv")
     for i in channels.index:
-        wavelength = channels.iloc[i]["Wavelength"]
-        channel = channels.iloc[i]["Channel"]
+        wavelength = channels.iloc[i]["wavelength"]
+        channel = channels.iloc[i]["channel"]
         ax.vlines(wavelength, *Y_LIM, 'k', ':')
         ax.text(wavelength+0.05, Y_LIM[0]+0.05, f"{int(channel)}", rotation=90)
 
 
 def plot_transmission_data(ax):
     """Plot transmission against frequency for specified csv files."""
-    file_paths = [RESONANCE_DIR / file for file in FILES]
-    for file_path in file_paths:
+    data = []
+    for file in FILES:
         datum = pd.read_csv(
-            file_path,
+            RESONANCE_DIR / file,
             header=None,
             names=["wavelength", "transmission"],
         )
+        data.append(datum)
+
+    # Find the highest transmission
+    # Adjust all sweeps to this for easier comparison
+    maxima = []
+    for datum in data:
+        maxima.append(datum["transmission"].max())
+    max_transmission = max(maxima)
+    for datum in data:
+        diff = max_transmission - datum["transmission"].max()
+        datum["transmission"] += diff
+
+    # Plot adjusted transmission data
+    for i, datum in enumerate(data):
+        file = FILES[i]
         datum.plot(
             kind='line',
             x="wavelength",
             y="transmission",
             ax=ax,
+            label=file[:-3],  # Remove file type from label
         )
 
 
@@ -61,12 +81,14 @@ def plot_dwdm_transmission(ax):
     """Plot transmission data for specified DWDM channels"""
     channel_number = None
     dwdm_filenames = []
+    channels = []
 
     for dwdm_filename in os.listdir(DWDM_DIR):
         try: channel_number = int(dwdm_filename[8:10])
-        except ValueError: pass
+        except ValueError: pass  # Some files will not have a channel number
         if channel_number in DWDM_CHANNELS:
             dwdm_filenames.append(dwdm_filename)
+            channels.append(channel_number)
 
     for i, filename in enumerate(dwdm_filenames):
         file_path = DWDM_DIR / filename
@@ -83,6 +105,7 @@ def plot_dwdm_transmission(ax):
             y="transmission",
             ax=ax,
             color=color,
+            label=f"channel {int(channels[i])}",
         )
 
         # Fill with color under band-pass filter transmission
