6a9839e2136500fcec30d0a7b312d8e3fd40b39e
Freddie Burns <freddieburns@hotmail.co.uk>
2023-12-09 13:58:22 +0000

    Pull script out of function

--- lightrig/sweep_wavelengths.py
+++ lightrig/sweep_wavelengths.py
@@ -10,29 +10,28 @@ from lightrig.instruments.powermeter import Powermeter
 from lightrig.instruments.sweeper import Sweeper
 
 
-def run():
-    # Wavelength sweep parameters
-    device_name = "ring-04-dwdm"
-    start_wavelength = 1553
-    end_wavelength = 1554
-    wavelength_step = 0.01
+# Wavelength sweep parameters
+device_name = "ring-04-dwdm"
+start_wavelength = 1553
+end_wavelength = 1554
+wavelength_step = 0.01
 
-    # Instrument parameters
-    powermeter_serial = 'P0001012'
-    laser_com_port = 7
-    laser_channel = 1
-    save_path = Path("../data/resonance")
+# Instrument parameters
+powermeter_serial = 'P0001012'
+laser_com_port = 7
+laser_channel = 1
+save_path = Path("../data/resonance")
 
-    # Connect to instruments
-    laser = Laser(COM_port=laser_com_port, channel=laser_channel)
-    powermeter = Powermeter(powermeter_serial)
+# Connect to instruments
+laser = Laser(COM_port=laser_com_port, channel=laser_channel)
+powermeter = Powermeter(powermeter_serial)
 
-    # Run wavelength sweep
-    sweeper = Sweeper(laser, powermeter)
-    sweeper.sweep(
-        save_path=save_path,
-        device_name=device_name,
-        start=start_wavelength,
-        end=end_wavelength,
-        step=wavelength_step,
-    )
+# Run wavelength sweep
+sweeper = Sweeper(laser, powermeter)
+sweeper.sweep(
+    save_path=save_path,
+    device_name=device_name,
+    start=start_wavelength,
+    end=end_wavelength,
+    step=wavelength_step,
+)
