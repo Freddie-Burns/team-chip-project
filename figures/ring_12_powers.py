c1705fc4330ab4c19ca9d78fe9daa764a030b541
Freddie Burns <freddieburns@hotmail.co.uk>
2023-12-14 19:33:47 +0000

    Plotting CAR vs power

---
+++ figures/ring_12_powers.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from matplotlib import pyplot as plt
+
+from utils import calculate_car
+
+DATA_DIR = Path("../data/swabian/coincidences")
+
+FILES = [
+    "ring-12 binwidth_10ps trigger_0.07V power_1dBm integration_120s "
+    "2023-12-13 11-22-28.csv",
+    "ring-12 binwidth_10ps trigger_0.07V power_2dBm integration_120s "
+    "2023-12-13 11-15-32.csv",
+    "ring-12 binwidth_10ps trigger_0.07V power_3dBm integration_120s "
+    "2023-12-13 11-12-05.csv",
+    "ring-12 binwidth_10ps trigger_0.07V power_4dBm integration_120s "
+    "2023-12-13 11-09-40.csv",
+    "ring-12 binwidth_10ps trigger_0.07V power_5dBm integration_120s "
+    "2023-12-13 11-07-15.csv",
+    "ring-12 binwidth_10ps trigger_0.07V power_6dBm integration_120s "
+    "2023-12-13 11-04-50.csv",
+    "ring-12 binwidth_10ps trigger_0.07V power_7dBm integration_180s "
+    "2023-12-13 10-57-31.csv",
+]
+
+powers = [1, 2, 3, 4, 5, 6, 7]
+cars = []
+for file in FILES:
+    filepath = DATA_DIR / file
+    cars.append(calculate_car(filepath))
+
+plt.scatter(powers, cars, marker='x')
+plt.xlabel("power / dBm")
+plt.ylabel("CAR")
+plt.show()
