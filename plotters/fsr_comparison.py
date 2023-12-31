c3e14bacb3990bee6136daed363adf996b71fc0a
Freddie Burns <freddieburns@hotmail.co.uk>
2023-12-31 16:58:35 +0000

    FSR plots
    Free spectral range

--- plotters/fsr_comparison.py
+++ plotters/fsr_comparison.py
@@ -3,21 +3,19 @@ from helpers import resonance
 
 
 channel_centres = range(29, 41)
-dwdm_channels = (30, 33, 35, 37, 40)
-files = ["ring-12 2023-12-13 10-04-47.csv"]
-labels = ["ring-12"]
+files = ["ring-01 2023-12-04 13-50-17.csv", "ring-24 2023-12-12 09-27-44.csv"]
+labels = ["ring-01", "ring-24"]
 xlim = (1545, 1554.5)
-ylim = (-15, -11)
+ylim = (-20.5, -17)
 
 
 plt.figure()
 ax = plt.subplot()
 resonance.plot_channel_centers(ax, channel_centres, ylim)
 resonance.plot_transmission_data(ax, files, labels)
-resonance.plot_dwdm_transmission(ax, dwdm_channels)
 plt.ylim(ylim)
 plt.xlim(xlim)
 plt.xlabel("wavelength / nm")
 plt.ylabel("transmission / dB")
-plt.savefig("figures/ring_12_resonances.png")
+plt.savefig("figures/fsr_comparison.png")
 plt.show()
